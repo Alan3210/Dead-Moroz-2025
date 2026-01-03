2c30a34dd51b27f672f753ff4df8d25a42a4ae61
Alan3210 <alan.kodzayev@gmail.com>
2026-01-03 15:45:47 +0300

    25. Start screen

--- Assets/Scripts/UI/BankingScreenController.cs
+++ Assets/Scripts/UI/BankingScreenController.cs
@@ -120,7 +120,8 @@ public class BankingScreenController : MonoBehaviour
         continueButton.gameObject.SetActive(false);
         loanButton.gameObject.SetActive(false);
 
-        headerText.text = $"РАСЧЁТ ЗА МЕСЯЦ {monthNumber}";
+        headerText.text = $"ЗАРПЛАТА ЗА {GetMonthNameInRussian(monthNumber).ToUpper()}";
+
 
         yield return new WaitForSeconds(0.5f);
 
@@ -291,6 +292,25 @@ public class BankingScreenController : MonoBehaviour
         if (backgroundOverlayAnimator != null)
         {
             backgroundOverlayAnimator.gameObject.SetActive(false);
+        }
+    }
+    private string GetMonthNameInRussian(int monthNumber)
+    {
+        switch (monthNumber)
+        {
+            case 1: return "январь";
+            case 2: return "февраль";
+            case 3: return "март";
+            case 4: return "апрель";
+            case 5: return "май";
+            case 6: return "июнь";
+            case 7: return "июль";
+            case 8: return "август";
+            case 9: return "сентябрь";
+            case 10: return "октябрь";
+            case 11: return "ноябрь";
+            case 12: return "декабрь";
+            default: return $"месяц {monthNumber}";
         }
     }
 
